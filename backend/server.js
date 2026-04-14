28dca6794394336fefdcb53f0167721c434de81a
Shaiju Raju <shaijur2014@gmail.com>
2026-04-14 13:01:38 +0530

    feat: implement authentication and setup real-time socket connection

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,29 @@
 import dotenv from "dotenv";
-import app from './app.js';
+dotenv.config();
 
+import app from "./app.js";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
+const server = createServer(app);
 
-dotenv.config(); 
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
 
-const PORT = process.env.PORT 
+io.on("connection", (socket) => {
+  console.log("🟢 User connected:", socket.id);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  socket.on("disconnect", () => {
+    console.log("🔴 User disconnected:", socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- frontend/pages/Chat.jsx
+++ frontend/pages/Chat.jsx
@@ -1,4 +1,21 @@
+import { useEffect } from "react";
+import {io} from "socket.io-client";
+const API_URL = import.meta.env.VITE_API_URL;
+
+const socket = io(API_URL);
+
+
 export default function Chat() {
+
+  useEffect ( ()=> {
+    socket.on("connect", () => {
+      console.log("Connected:", socket.id);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  },[]);
   return (
     <div>
       <h1>Welcome to Chat App 🚀</h1>
